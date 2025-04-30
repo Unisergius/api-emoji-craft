5dab5799d1ba7ee8561ead25fd6ec1b5034c8547
shenriquesdev <Unisergius@gmail.com>
2025-04-30 20:09:48 +0100

    bugfix - remove ecto from tests

--- config/test.exs
+++ config/test.exs
@@ -1,17 +1,6 @@
 import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
-config :api_emoji_craft, ApiEmojiCraft.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "api_emoji_craft_test#{System.get_env("MIX_TEST_PARTITION")}",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+
 
 # We don't run a server during test. If one is required,ß
 # you can enable the server option below.
@@ -22,9 +11,6 @@ config :api_emoji_craft, ApiEmojiCraftWeb.Endpoint,
 
 # In test we don't send emails.
 config :api_emoji_craft, ApiEmojiCraft.Mailer, adapter: Swoosh.Adapters.Test
-
-# Disable swoosh api client as it is only required for production adapters.
-config :swoosh, :api_client, false
 
 # Print only warnings and errors during test
 config :logger, level: :warning
--- mix.exs
+++ mix.exs
@@ -48,10 +48,8 @@ defmodule ApiEmojiCraft.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      setup: ["deps.get"],
+      test: ["test"]
     ]
   end
 end
